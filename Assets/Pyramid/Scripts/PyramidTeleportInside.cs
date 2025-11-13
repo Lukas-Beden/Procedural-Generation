0049d51e16bcc14b142b36aa25e2e60a2008f362
Enzo <130132864+Nyevius@users.noreply.github.com>
2025-11-13 14:26:28 +0100

    [ADD] Both inside and outside pyramids

---
+++ Assets/Pyramid/Scripts/PyramidTeleportInside.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel;
+using UnityEngine;
+
+public class PyramidTeleportInside : MonoBehaviour
+{
+    private GameObject indoorSpawnPoint;
+    [SerializeField] private GameObject pyramidIndoors;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Instantiate(pyramidIndoors, new Vector3(1000,0,1000), Quaternion.identity);
+            
+            indoorSpawnPoint = GameObject.Find("Indoor Spawnpoint");
+
+            other.gameObject.transform.SetPositionAndRotation(indoorSpawnPoint.transform.position, Quaternion.identity);
+            other.gameObject.transform.Rotate(new Vector3(0,180,0), Space.World);
+        }
+        else
+        {
+            Debug.LogError("Collide avec autre chose qu'un joueur");
+        }
+    }
+}
